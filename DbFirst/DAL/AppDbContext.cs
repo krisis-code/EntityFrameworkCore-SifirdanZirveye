10fa903143ac8f05ff798f2346fdf21c54ba2b07
Enes Başpınar <Menesbaspinar@hotmail.com>
2024-11-02 23:49:23 +0300

    db connection string eklendi

--- DbFirst/DAL/AppDbContext.cs
+++ DbFirst/DAL/AppDbContext.cs
@@ -1,14 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.SqlServer;
 
 namespace DbFirst.DAL
 {
     public class AppDbContext : DbContext
     {
         public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=45.84.189.34\\MSSQLSERVER2022;Initial Catalog=enesbasp_Kurs;User ID=enesbasp_enesbaspinar;Password=Swgk25413789*;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+
+        }
     }
 }
