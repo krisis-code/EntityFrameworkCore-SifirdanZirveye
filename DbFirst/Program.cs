8d4f80031d1191ad62614714edfa9884c2346424
Enes Başpınar <Menesbaspinar@hotmail.com>
2024-11-03 23:56:34 +0300

    Data listelendi

--- DbFirst/Program.cs
+++ DbFirst/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using DbFirst.DAL;
+using Microsoft.EntityFrameworkCore;
+
+using (var _context = new AppDbContext())
+{
+    var product = await _context.Products.ToListAsync();
+
+    Console.WriteLine("Id : İsim");
+    product.ForEach(p => 
+    {
+        
+
+        Console.WriteLine($"{p.Id}  : {p.Name}");
+
+    });
+    Console.WriteLine("Id : İsim");
+}
