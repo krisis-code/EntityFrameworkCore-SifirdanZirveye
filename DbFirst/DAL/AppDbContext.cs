c442c75603439e53e0f17c79349fce0f34191393
Enes Başpınar <Menesbaspinar@hotmail.com>
2024-11-05 00:02:20 +0300

    Appsettings eklendi bağlantı strieng yazıldı

--- DbFirst/DAL/AppDbContext.cs
+++ DbFirst/DAL/AppDbContext.cs
@@ -9,7 +9,10 @@ namespace DbFirst.DAL
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=45.84.189.34\\MSSQLSERVER2022;Initial Catalog=enesbasp_Kurs;User ID=enesbasp_enesbaspinar;Password=Swgk25413789*;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+            optionsBuilder.UseSqlServer("Data Source=45.84.189.34\\" +
+                "MSSQLSERVER2022;Initial Catalog=enesbasp_Kurs;User " +
+                "ID=enesbasp_enesbaspinar;Password=Swgk25413789*;" +
+                "Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
 
         }
     }
