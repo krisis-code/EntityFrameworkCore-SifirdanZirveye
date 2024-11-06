1eae6d37878d2b68751d02ebd57a15bc510f12e0
Enes Başpınar <Menesbaspinar@hotmail.com>
2024-11-07 00:50:35 +0300

    appsetting işlemleri bitti

--- DbFirst/Program.cs
+++ DbFirst/Program.cs
@@ -3,7 +3,9 @@
 using DbFirst.DAL;
 using Microsoft.EntityFrameworkCore;
 
-using (var _context = new AppDbContext())
+DbContextInitializer.Build();
+
+using (var _context = new AppDbContext(DbContextInitializer.optionsBuilder.Options))
 {
     var product = await _context.Products.ToListAsync();
 
